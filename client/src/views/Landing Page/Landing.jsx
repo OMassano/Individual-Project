53c9b48fc60eeec57fa8a41c3ef59d5047084bd5
oceanamassano <oshimassano@gmail.com>
2023-05-15 10:19:11 -0300

    some edit to styles

--- client/src/views/Landing Page/Landing.jsx
+++ client/src/views/Landing Page/Landing.jsx
@@ -2,7 +2,7 @@ import style from "./Landing.module.css"
 const Landing = ()=>{
     return(
         <div className={style.background}>
-        <h1>Welcome to the official dog API</h1>
+        <h1 className={style.landing}>Welcome to the official dog API</h1>
         <p>To access site please click on the button below</p>
         <a href="/homepage" className={style.button}>
             Click Me
