101e28227efbd2c215324bf47681536dda68f4e1
oceanamassano <oshimassano@gmail.com>
2023-05-22 18:50:54 -0300

    temp problem = solved

--- api/src/allDogs.js
+++ api/src/allDogs.js
@@ -2,16 +2,17 @@ const { Dog, Temperament } = require("./db");
 const axios = require("axios");
 const { API_KEY, API_URL } = process.env;
 
-// filtering data from my response in api to return the specific data im asking for, which will be put in the object
 const filterDogsData = async (res) => {
-  const filteredDogs = await res.data.map((dog) => ({
+  const filteredDogs = res.data.map((dog) => ({
     id: dog.id,
     name: dog.name,
     image: dog.image.url,
     height: dog.height.imperial,
     weight: dog.weight.imperial,
     life_span: dog.life_span,
-    temperament: dog.temperament?.split(",").map((temp) => temp.trim()) || [],
+    temperament: dog.temperament
+      ? dog.temperament.split(",").map((temp) => temp.trim())
+      : [],
   }));
   return filteredDogs;
 };
@@ -19,13 +20,23 @@ const filterDogsData = async (res) => {
 const allDogs = async () => {
   const res = await axios.get(`${API_URL}?apikey=${API_KEY}`);
   const dogsInApi = await filterDogsData(res);
+
   const dogsInDb = await Dog.findAll({
     include: {
       model: Temperament,
       attributes: ["name"],
     },
   });
-  const dogsInDbValues = dogsInDb.map((dog) => dog.get({ plain: true }));
+
+  const dogsInDbValues = dogsInDb.map((dog) => ({
+    id: dog.id,
+    name: dog.name,
+    image: dog.image,
+    height: dog.height,
+    weight: dog.weight,
+    life_span: dog.life_span,
+    temperament: dog.Temperaments.map((temp) => temp.name),
+  }));
 
   const allDogs = [...dogsInApi, ...dogsInDbValues];
   allDogs.sort((a, b) => a.name.localeCompare(b.name));
--- api/src/handlers/postNewDogHandler.js
+++ api/src/handlers/postNewDogHandler.js
@@ -1,11 +1,14 @@
 const { Dog, Temperament } = require("../db");
+
 const postNewDogHandler = async (req, res) => {
-  const { name, image, height, weight, life_span, temperaments } = req.body;
+  const { name, image, height, weight, life_span, temperament } = req.body;
+
   try {
     const newDog = await Dog.create({ name, image, height, weight, life_span });
 
+    const tempNames = Array.isArray(temperament) ? temperament : [temperament];
     const tempInDb = await Temperament.findAll({
-      where: { name: temperaments },
+      where: { name: tempNames },
     });
 
     await newDog.addTemperaments(tempInDb); // creating relationship
@@ -26,4 +29,5 @@ const postNewDogHandler = async (req, res) => {
     res.status(404).json({ error: error.message });
   }
 };
+
 module.exports = postNewDogHandler;
