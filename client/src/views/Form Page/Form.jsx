0f2787436d8a55cbaea5f97974efcfc73fe38bcf
oceanamassano <oshimassano@gmail.com>
2023-05-23 11:46:07 -0300

    temps dropdown, both working

--- client/src/views/Form Page/Form.jsx
+++ client/src/views/Form Page/Form.jsx
@@ -5,6 +5,11 @@ import { getTemps } from "../../redux/action";
 
 const Form = () => {
   const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(getTemps())
+  })
+
   const [form, setForm] = useState({
     name: "",
     image: "",
@@ -15,6 +20,8 @@ const Form = () => {
     maxWeight: "",
     weight: "",
     life_span: "",
+    temp1: "",
+    temp2: "",
     temperament: [],
   });
 
@@ -32,10 +39,6 @@ const Form = () => {
     const property = event.target.name; // place in form
     let value = event.target.value; // whats written in form
 
-    if (property === "temperament") {
-      value = value.split(",").map((temp) => temp.trim());
-    }
-
     validate({ ...form, [property]: value });
     setForm({ ...form, [property]: value });
   };
@@ -45,26 +48,19 @@ const Form = () => {
   const submitHandler = (event) => {
     event.preventDefault();
 
-    const { minWeight, maxWeight, minHeight, maxHeight, ...rest } = form;
+    const {minWeight,maxWeight,minHeight,maxHeight,temp1,temp2,...rest} = form;
     const weight = `${minWeight}-${maxWeight}`;
     const height = `${minHeight} - ${maxHeight}`;
-    const formData = { ...rest, weight, height };
+    const temperament = [temp1, temp2];
+    const formData = { ...rest, weight, height, temperament };
     console.log(formData);
-    axios
-      .post("http://localhost:5002/dogs", formData)
-      .then(() =>
-        alert(
-          `${form.name} was created successfully and is now in our dog database!`
-        )
-      )
+
+    axios.post("http://localhost:5002/dogs", formData)
+      .then(() =>alert(`${form.name} was created successfully and is now in our dog database!`))
       .catch((error) => alert(error));
   };
-  useEffect(() =>{
-    dispatch(getTemps())
-  }, [])
-  // const viewTemps = () => {
-  //   dispatch(getTemps());
-  // };
+
+
   const temperaments = useSelector((state) => state.temperaments);
 
   return (
@@ -80,67 +76,41 @@ const Form = () => {
       </div>
       <div>
         <label>Image URL: </label>
-        <input
-          type="text"
-          value={form.image}
-          name="image"
-          onChange={changeHandler}
-        />
+        <input type="text" value={form.image} name="image" onChange={changeHandler} />
       </div>
       <div>
         <label>Minimum Height: </label>
-        <input
-          type="text"
-          value={form.minHeight}
-          name="minHeight"
-          onChange={changeHandler}
-        />
+        <input type="text" value={form.minHeight} name="minHeight" onChange={changeHandler}/>
       </div>
       <div>
         <label>Maximum Height: </label>
-        <input
-          type="text"
-          value={form.maxHeight}
-          name="maxHeight"
-          onChange={changeHandler}
-        />
+        <input type="text" value={form.maxHeight} name="maxHeight"onChange={changeHandler}/>
       </div>
       <div>
         <label>Minimum weight: </label>
-        <input
-          type="text"
-          value={form.minWeight}
-          name="minWeight"
-          onChange={changeHandler}
-        />
+        <input type="text" value={form.minWeight} name="minWeight" onChange={changeHandler}/>
       </div>
       <div>
         <label>Maximum weight: </label>
-        <input
-          type="text"
-          value={form.maxWeight}
-          name="maxWeight"
-          onChange={changeHandler}
-        />
+        <input type="text" value={form.maxWeight} name="maxWeight" onChange={changeHandler}/>
       </div>
       <div>
         <label>LifeSpan: </label>
-        <input
-          type="text"
-          value={form.life_span}
-          name="life_span"
-          onChange={changeHandler}
-        />
+        <input type="text" value={form.life_span} name="life_span" onChange={changeHandler}/>
       </div>
       <div>
-        <label>Temperaments: </label>
-        <select
-          multiple
-          value={form.temperament}
-          name="temperament"
-          onChange={changeHandler}
-          // onClick={viewTemps}
-        >
+        <label>First Temperament: </label>
+        <select value={form.temp1} name="temp1" onChange={changeHandler}>
+          {temperaments &&
+            temperaments.map((temp) =>
+              temp.name ? <option key={temp.id}>{temp.name}</option> : null
+            )}
+        </select>
+      </div>
+
+      <div>
+        <label>Second Temperament: </label>
+        <select value={form.temp2} name="temp2" onChange={changeHandler}>
           {temperaments &&
             temperaments.map((temp) =>
               temp.name ? <option key={temp.id}>{temp.name}</option> : null
