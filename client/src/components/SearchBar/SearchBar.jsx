1e69a45cbdad9c143ed827395e65df965d64a84b
oceanamassano <oshimassano@gmail.com>
2023-05-24 16:46:14 -0300

    timeout working

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import { getDogByName } from "../../redux/action";
-import style from "./SearchBar.module.css"
+import style from "./SearchBar.module.css";
 
 const SearchBar = () => {
   const dispatch = useDispatch();
@@ -14,25 +14,39 @@ const SearchBar = () => {
 
   const dogs = useSelector((state) => state.dogs);
 
+  let searchTimeout;
+
   const onSearch = () => {
     const existingDog = dogs.find(
-      (dog) => dog.name.toLowerCase() === name.toLowerCase() //checking to see if i already searched up dog
+      (dog) => dog.name.toLowerCase() === name.toLowerCase()
     );
+
     if (!existingDog) {
-      dispatch(getDogByName(name)).catch(() => {
-        window.alert(
-          "That dog does not exist! Please attempt to search for a different dog breed or add the dog yourself in our new form section!"
-        );
-      });
+      clearTimeout(searchTimeout);
+
+      // Start a new timeout so that it wont load dogs twice
+      searchTimeout = setTimeout(() => {
+        dispatch(getDogByName(name)).catch(() => {
+          window.alert(
+            "That dog does not exist! Please attempt to search for a different dog breed or add the dog yourself in our new form section!"
+          );
+        });
+      }, 100);
     } else {
       alert("You have already searched up that name!");
     }
   };
-
   return (
     <div>
-      <input type="search" onChange={handleChange} value={name} className={style.bar}/>
-      <button onClick={onSearch} className={style.button}>Search by Breed</button>
+      <input
+        type="search"
+        onChange={handleChange}
+        value={name}
+        className={style.bar}
+      />
+      <button onClick={onSearch} className={style.button}>
+        Search by Breed
+      </button>
     </div>
   );
 };
