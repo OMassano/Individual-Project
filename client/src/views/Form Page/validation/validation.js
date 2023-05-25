30316c7df0773df568124c4ae6fe6625a18acd38
oceanamassano <oshimassano@gmail.com>
2023-05-25 11:33:00 -0300

    random

--- client/src/views/Form Page/validation/validation.js
+++ client/src/views/Form Page/validation/validation.js
@@ -1,70 +1,63 @@
-import axios from "axios"
-const validate = async (form) => {
+const validate = (form) => {
     const errors = {};
-
-    const dogs = await axios.get("http://localhost:5002/getalldogs").data
-    console.log(dogs)
-    const existingDog = dogs.find(dog=>dog.name === form.name)
-  if (!form.name) {
-    errors.name = "Please enter a dog breed/name";
-  } else if (!/^[A-Za-z\s]+$/.test(form.name)) {
-    errors.name =
-      "The breed/name must only contain letters and spaces; Please enter a valid name";
-  } else if (form.name.length > 30) {
-    errors.name = "The breed/name must contain no more than 30 characters";
+  
+    if (!form.name) {
+      errors.name = "Please enter a dog breed/name";
+    } else if (!/^[A-Za-z\s]+$/.test(form.name)) {
+      errors.name =
+        "The breed/name must only contain letters and spaces; Please enter a valid name";
+    } else if (form.name.length > 30) {
+      errors.name = "The breed/name must contain no more than 30 characters";
+    }
+  //---------------------------------------------------------------------------------------------------------------------------------
+    if (!form.minHeight) {
+      errors.minHeight = "Please enter a minimum height";
+    } else if (isNaN(form.minHeight)) {
+      errors.minHeight = "Minimum height must be a valid number";
+    } else if (Number(form.minHeight) < 5 || Number(form.minHeight) > 40) {
+      errors.minHeight = "Height must be within 5 and 40 inches";
+    }
+  
+    if (!form.maxHeight) {
+      errors.maxHeight = "Please enter a maximum height";
+    } else if (isNaN(form.maxHeight)) {
+      errors.maxHeight = "Maximum height must be a valid number";
+    } else if (Number(form.maxHeight) < 5 || Number(form.maxHeight) > 40) {
+      errors.maxHeight = "Height must be within 5 and 40 inches";
+    } else if (Number(form.minHeight) >= Number(form.maxHeight)) {
+      errors.maxHeight =
+        "Minimum height cannot be greater than or equal to maximum height. Please ensure your minimum is a lesser number than your maximum";
+    }
+  //---------------------------------------------------------------------------------------------------------------------------------
+  if (!form.minWeight) {
+      errors.minWeight = "Please enter a minimum weight";
+    } else if (isNaN(form.minWeight)) {
+      errors.minWeight = "Minimum weight must be a valid number";
+    } else if (Number(form.minWeight) < 3 || Number(form.minWeight) > 250) {
+      errors.minWeight = "Weight must be between 3 and 250 lbs";
+    }
+  
+    if (!form.maxWeight) {
+      errors.maxWeight = "Please enter a maximum weight";
+    } else if (isNaN(form.maxWeight)) {
+      errors.maxWeight = "Maximum weight must be a valid number";
+    } else if (Number(form.maxWeight) < 3 || Number(form.maxWeight) > 250) {
+      errors.maxWeight = "Weight must be between 3 and 250 lbs";
+    } else if (Number(form.minWeight) >= Number(form.maxWeight)) {
+      errors.maxWeight =
+        "Minimum weight cannot be greater than or equal to maximum weight. Please ensure your minimum is a lesser number than your maximum";
+    }
+  //---------------------------------------------------------------------------------------------------------------------------------
+  if (!form.life_span) {
+      errors.life_span = "Please enter the lifespan in the format 'x years'.";
+    } else if (!/^\d+\s+years$/.test(form.life_span)) {
+      errors.life_span = "Invalid format. Enter the lifespan in the format 'x years'. X needs to be a valid number";
+    }
+  //---------------------------------------------------------------------------------------------------------------------------------
+  if(!form.temp1 || !form.temp2){
+      errors.temp1= "Please select two temperaments"
   }
-  else if(existingDog){
-    errors.name = "That dog already exists!"
-  }
-//---------------------------------------------------------------------------------------------------------------------------------
-  if (!form.minHeight) {
-    errors.minHeight = "Please enter a minimum height";
-  } else if (isNaN(form.minHeight)) {
-    errors.minHeight = "Minimum height must be a valid number";
-  } else if (Number(form.minHeight) < 5 || Number(form.minHeight) > 40) {
-    errors.minHeight = "Height must be within 5 and 40 inches";
-  }
-
-  if (!form.maxHeight) {
-    errors.maxHeight = "Please enter a maximum height";
-  } else if (isNaN(form.maxHeight)) {
-    errors.maxHeight = "Maximum height must be a valid number";
-  } else if (Number(form.maxHeight) < 5 || Number(form.maxHeight) > 40) {
-    errors.maxHeight = "Height must be within 5 and 40 inches";
-  } else if (Number(form.minHeight) >= Number(form.maxHeight)) {
-    errors.maxHeight =
-      "Minimum height cannot be greater than or equal to maximum height. Please ensure your minimum is a lesser number than your maximum";
-  }
-//---------------------------------------------------------------------------------------------------------------------------------
-if (!form.minWeight) {
-    errors.minWeight = "Please enter a minimum weight";
-  } else if (isNaN(form.minWeight)) {
-    errors.minWeight = "Minimum weight must be a valid number";
-  } else if (Number(form.minWeight) < 3 || Number(form.minWeight) > 250) {
-    errors.minWeight = "Weight must be between 3 and 250 lbs";
-  }
-
-  if (!form.maxWeight) {
-    errors.maxWeight = "Please enter a maximum weight";
-  } else if (isNaN(form.maxWeight)) {
-    errors.maxWeight = "Maximum weight must be a valid number";
-  } else if (Number(form.maxWeight) < 3 || Number(form.maxWeight) > 250) {
-    errors.maxWeight = "Weight must be between 3 and 250 lbs";
-  } else if (Number(form.minWeight) >= Number(form.maxWeight)) {
-    errors.maxWeight =
-      "Minimum weight cannot be greater than or equal to maximum weight. Please ensure your minimum is a lesser number than your maximum";
-  }
-//---------------------------------------------------------------------------------------------------------------------------------
-if (!form.life_span) {
-    errors.life_span = "Please enter the lifespan in the format 'x years'.";
-  } else if (!/^\d+\s+years$/.test(form.life_span)) {
-    errors.life_span = "Invalid format. Enter the lifespan in the format 'x years'. X needs to be a valid number";
-  }
-//---------------------------------------------------------------------------------------------------------------------------------
-if(!form.temp1 || !form.temp2){
-    errors.temp1= "Please select two temperaments"
-}
-  return errors;
-};
-
-export default validate;
+    return errors;
+  };
+  
+  export default validate;
