a0d5da00b9f9a2be7c567c8a0ebebdfe72bd0ba0
oceanamassano <oshimassano@gmail.com>
2023-05-26 13:22:18 -0300

    styles form

--- client/src/views/Form Page/Form.jsx
+++ client/src/views/Form Page/Form.jsx
@@ -3,13 +3,14 @@ import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { getTemps } from "../../redux/action";
 import validate from "./validation/validation";
+import style from "./Form.module.css";
 
 const Form = () => {
   const dispatch = useDispatch();
 
-  useEffect(()=>{
-    dispatch(getTemps())
-  })
+  useEffect(() => {
+    dispatch(getTemps());
+  });
 
   const temperaments = useSelector((state) => state.temperaments);
 
@@ -49,7 +50,7 @@ const Form = () => {
     let value = event.target.value; // whats written in form
 
     if (property === "name") {
-      value = value.replace(/\b\w/g, (c) => c.toUpperCase());// sets letter of each word to caps
+      value = value.replace(/\b\w/g, (c) => c.toUpperCase()); // sets letter of each word to caps
     }
 
     setErrors(validate({ ...form, [property]: value }));
@@ -60,84 +61,138 @@ const Form = () => {
   const submitHandler = (event) => {
     event.preventDefault();
 
-    const {minWeight,maxWeight,minHeight,maxHeight,temp1,temp2, ...rest} = form;
+    const {
+      minWeight,
+      maxWeight,
+      minHeight,
+      maxHeight,
+      temp1,
+      temp2,
+      ...rest
+    } = form;
     const weight = `${minWeight}-${maxWeight}`;
     const height = `${minHeight} - ${maxHeight}`;
     const temperament = [temp1, temp2];
     const formData = { ...rest, weight, height, temperament };
     console.log(formData);
-    if(errors){
-      alert("Please resolve any errors before submitting this form")
-    }else{
-      axios.post("http://localhost:5002/dogs", formData)
-        .then(() =>alert(`${form.name} was created successfully and is now in our dog database!`))
+    if (errors) {
+      alert("Please resolve any errors before submitting this form");
+    } else {
+      axios
+        .post("http://localhost:5002/dogs", formData)
+        .then(() =>
+          alert(
+            `${form.name} was created successfully and is now in our dog database!`
+          )
+        )
         .catch((error) => alert(error));
     }
   };
 
-
   return (
-    <form onSubmit={submitHandler}>
-      <div>
-        <label>Breed: </label>
-        <input
-          type="text"
-          value={form.name}
-          name="name"
-          onChange={changeHandler}
-        />
-        {errors.name && <p>{errors.name}</p>}
-      </div>
-      <div>
-        <label>Image URL: </label>
-        <input type="text" value={form.image} name="image" onChange={changeHandler} />
-      </div>
-      <div>
-        <label>Height (inches): </label>
-        <input type="text" value={form.minHeight} name="minHeight" onChange={changeHandler} placeholder="Minimum"/>
-        {errors.minHeight && <p>{errors.minHeight}</p>}
-      </div>
-      <div>
-        <input type="text" value={form.maxHeight} name="maxHeight"onChange={changeHandler} placeholder="Maximum"/>
-        {errors.maxHeight && <p>{errors.maxHeight}</p>}
-      </div>
-      <div>
-        <label>Weight (lbs): </label>
-        <input type="text" value={form.minWeight} name="minWeight" onChange={changeHandler} placeholder="Minimum"/>
-        {errors.minWeight && <p>{errors.minWeight}</p>}
-      </div>
-      <div>
-        <input type="text" value={form.maxWeight} name="maxWeight" onChange={changeHandler} placeholder="Maximum"/>
-        {errors.maxWeight && <p>{errors.maxWeight}</p>}
-      </div>
-      <div>
-        <label>LifeSpan: </label>
-        <input type="text" value={form.life_span} name="life_span" onChange={changeHandler} placeholder="Years"/>
-        {errors.life_span && <p>{errors.life_span}</p>}
-      </div>
-      <div>
-        <label>First Temperament: </label>
-        <select value={form.temp1} name="temp1" onChange={changeHandler}>
-          {temperaments &&
-            temperaments.map((temp) =>
-              temp.name ? <option key={temp.id}>{temp.name}</option> : null
-            )}
-        </select>
-      </div>
+    <div>
+      <form onSubmit={submitHandler} className={style.container}>
+      <h1>Create Your Own Dog</h1>
+        <div>
+          <label>Breed: </label>
+          <input
+            type="text"
+            value={form.name}
+            name="name"
+            onChange={changeHandler}
+          />
+          {errors.name && <p>{errors.name}</p>}
+        </div>
+        <div>
+          <label>Image URL: </label>
+          <input
+            type="text"
+            value={form.image}
+            name="image"
+            onChange={changeHandler}
+          />
+        </div>
+        <div>
+          <label>Height (inches): </label>
+          <input
+            type="text"
+            value={form.minHeight}
+            name="minHeight"
+            onChange={changeHandler}
+            placeholder="Minimum"
+          />
+          {errors.minHeight && <p>{errors.minHeight}</p>}
+        </div>
+        <div>
+          <input
+            type="text"
+            value={form.maxHeight}
+            name="maxHeight"
+            onChange={changeHandler}
+            placeholder="Maximum"
+            className={style.maxinput}
+          />
+          {errors.maxHeight && <p>{errors.maxHeight}</p>}
+        </div>
+        <div>
+          <label>Weight (lbs): </label>
+          <input
+            type="text"
+            value={form.minWeight}
+            name="minWeight"
+            onChange={changeHandler}
+            placeholder="Minimum"
+          />
+          {errors.minWeight && <p>{errors.minWeight}</p>}
+        </div>
+        <div>
+          <input
+            type="text"
+            value={form.maxWeight}
+            name="maxWeight"
+            onChange={changeHandler}
+            placeholder="Maximum"
+            className={style.maxinput}
+          />
+          {errors.maxWeight && <p>{errors.maxWeight}</p>}
+        </div>
+        <div>
+          <label>LifeSpan: </label>
+          <input
+            type="text"
+            value={form.life_span}
+            name="life_span"
+            onChange={changeHandler}
+            placeholder="Years"
+          />
+          {errors.life_span && <p>{errors.life_span}</p>}
+        </div>
+        <div>
+          <label>First Temperament: </label>
+          <select value={form.temp1} name="temp1" onChange={changeHandler}>
+            {temperaments &&
+              temperaments.map((temp) =>
+                temp.name ? <option key={temp.id}>{temp.name}</option> : null
+              )}
+          </select>
+        </div>
 
-      <div>
-        <label>Second Temperament: </label>
-        <select value={form.temp2} name="temp2" onChange={changeHandler}>
-          {temperaments &&
-            temperaments.map((temp) =>
-              temp.name ? <option key={temp.id}>{temp.name}</option> : null
-            )}
-        </select>
-        {errors.temp1 && <p>{errors.temp1}</p>}
-      </div>
+        <div>
+          <label>Second Temperament: </label>
+          <select value={form.temp2} name="temp2" onChange={changeHandler}>
+            {temperaments &&
+              temperaments.map((temp) =>
+                temp.name ? <option key={temp.id}>{temp.name}</option> : null
+              )}
+          </select>
+          {errors.temp1 && <p>{errors.temp1}</p>}
+        </div>
 
-      <button type="submit">SUBMIT</button>
-    </form>
+        <button type="submit" className={style.button}>
+          SUBMIT
+        </button>
+      </form>
+    </div>
   );
 };
 
